ed3e8233d77879ec4b58ea090b67b25bf247b1d8
Fabio Barbosa <fabiio403@gmail.com>
2024-01-01 18:47:39 -0300

    formulario-validado

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ menuMobile.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
   menuMobile.classList.remove('menu-ativado');
 });
+// validacao formulario
+
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  nameValidate();
+  emailValidate();
+});
+
+function setError(index) {
+  campos[index].style.border = '2px solid #e63636';
+  spans[index].style.display = 'block';
+}
+function removeError(index) {
+  campos[index].style.border = '';
+  spans[index].style.display = 'none';
+}
+
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
+}
